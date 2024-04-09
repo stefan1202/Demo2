15411f6f3c4f25c114c83b723b6089d59439aa24
StefanD <stefan.durla@gmail.com>
2024-04-09 18:28:23 +0300

    commented

--- src/main/java/ro/sda/java64/demo2/components/TestProperties.java
+++ src/main/java/ro/sda/java64/demo2/components/TestProperties.java
@@ -1,28 +1,28 @@
-package ro.sda.java64.demo2.components;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-@Component
-@Profile("!dev")
-public class TestProperties implements CommandLineRunner {
-
-    @Value("${testConfig:Bleah}")
-    private String value;
-
-    @Value("${env.value}")
-    private String value2;
-
-    @Autowired
-    private ConfigPropEx configPropEx;
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Am initalizat valoarea cu:" + value);
-        System.out.println("Am initalizat env value cu:" + value2);
-        System.out.println("Configuration Props" + configPropEx);
-    }
-}
+//package ro.sda.java64.demo2.components;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Profile("!dev")
+//public class TestProperties implements CommandLineRunner {
+//
+//    @Value("${testConfig:Bleah}")
+//    private String value;
+//
+//    @Value("${env.value}")
+//    private String value2;
+//
+//    @Autowired
+//    private ConfigPropEx configPropEx;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("Am initalizat valoarea cu:" + value);
+//        System.out.println("Am initalizat env value cu:" + value2);
+//        System.out.println("Configuration Props" + configPropEx);
+//    }
+//}
